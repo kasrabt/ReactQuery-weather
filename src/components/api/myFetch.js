9bcd853e3904cd9d9d4919a15d95bc649b4e476d
kasra <macbookappleidkasra@gmail.com>
2023-06-03 19:06:52 +0330

    Add axios

--- src/components/api/myFetch.js
+++ src/components/api/myFetch.js
@@ -1,11 +1,13 @@
-export const FetchMydata = (ct ) => async () => {
-    
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${ct}&units=imperial&appid=5a8215f577076017ce8c00cf0fa1d07a`
-  );
-  if(!response.ok){
-    throw new Error('cant')
-  }
-  const data = response.json();
-  return data;
-};
+import axios from "axios";
+export const FetchMydata = (ct) => async () => {
+
+const response = await axios({
+    method: "get",
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${ct}&units=imperial&appid=5a8215f577076017ce8c00cf0fa1d07a`,
+  })
+   
+  const data  = response.data ; 
+  return data ;
+ 
+
+}
